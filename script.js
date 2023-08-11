31cc61cc212e8ae9c7e18406f528d8cd548ab21d
Debanga Sharma <debangasharma21@gmail.com>
2023-08-11 19:19:55 +0530

    Quantity Field Added

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ function calculateConsumption() {
 
     appliances.forEach(appliance => {
         const row = appliance.closest("tr");
+        const quantityField = row.querySelector("input[type='text'][name='quantity']");
         const powerField = row.querySelector("input[type='text'][name$='_power'], select[name$='_power']");
         const hourField = row.querySelector("input[type='text'][name$='_hour']");
         const minuteField = row.querySelector("input[type='text'][name$='_minute']");
        
+        let quantity = parseFloat(quantityField.value);
         const power = parseFloat(powerField.value);
         let hour = parseFloat(hourField.value);
         let minute = parseFloat(minuteField.value);
+
+        if(isNaN(quantity)){
+            quantity = 1;
+        }
 
         if (isNaN(hour)){
             hour = 0;
@@ -24,7 +30,7 @@ function calculateConsumption() {
         totalMinutes = (hour * 60) + minute;
 
         if (!isNaN(power) && !isNaN(totalMinutes)) {
-            totalUnits += (power * totalMinutes) / (1000 * 60);
+            totalUnits += (quantity * power * totalMinutes) / (1000 * 60);
         }
     });
 
@@ -33,19 +39,9 @@ function calculateConsumption() {
 
 function displayResult(totalUnits) {
     const resultDiv = document.querySelector("#total_result");
-    resultDiv.textContent = `Total Electricity Consumed Today: ${totalUnits.toFixed(2)} Units`;
-
-    // Monthly feature Added
-    const earlierResult = totalUnits; 
-    const multipliedResult = earlierResult * 30;
-
-    
-    const multipliedResultElement = document.createElement("p");
-    multipliedResultElement.textContent = `Average Electricity Consumed Monthly: ${multipliedResult.toFixed(2)} Units`;
-    
-    resultDiv.appendChild(multipliedResultElement);
-
-    
+    resultDiv.textContent = `Total Electricity Consumed Per Day: ${totalUnits.toFixed(2)} Units`;
+    const monthlyDiv = document.querySelector("#monthly_result");
+    monthlyDiv.textContent = `Average Electricity Consumed Per Month: ${(totalUnits * 30).toFixed(2)} Units`;    
 }
 
 function addApplianceRow() {
@@ -57,10 +53,10 @@ function addApplianceRow() {
             <input type="checkbox" name="device" value="new_appliance" checked>
             <input type="text" name="new_appliance" placeholder="Name of Appliance" class="otherAppliance">
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_appliance_power"></td>
         <td><input type="text" name="new_appliance_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_appliance_minute" placeholder="Minute"></td>
-
     `;
 
     table.appendChild(newRow);
@@ -79,6 +75,7 @@ function addLight(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Light</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td>
             <select id="lightWatt" name="new_light_power">
             <option value="5">5W</option>
@@ -108,6 +105,7 @@ function addFan(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Fan</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_fan_power" value="75W"></td>
         <td><input type="text" name="new_fan_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_fan_minute" placeholder="Minute"></td>
@@ -126,6 +124,7 @@ function addRefrigerator(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Refrigerator</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_refrigerator_power" value="300W"></td>
         <td><input type="text" name="new_refrigerator_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_refrigerator_minute" placeholder="Minute"></td>
@@ -144,6 +143,7 @@ function addTelevision(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Television</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_television_power" value="60W"></td>
         <td><input type="text" name="new_television_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_television_minute" placeholder="Minute"></td>
@@ -162,6 +162,7 @@ function addAirConditioner(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Air Conditioner</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td>                    
             <select id="airconditionerWatt" name="new_airconditioner_power">
                 <option value="1200">1200W</option>
@@ -189,6 +190,7 @@ function addGeyser(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Geyser</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_geyser_power" value="2200W"></td>
         <td><input type="text" name="new_geyser_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_geyser_minute" placeholder="Minute"></td>
@@ -207,6 +209,7 @@ function addOven(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Microwave Oven</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_oven_power" value="1400W"></td>
         <td><input type="text" name="new_oven_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_oven_minute" placeholder="Minute"></td>
@@ -225,6 +228,7 @@ function addWaterPump(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Water Pump</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_waterpump_power" value="800W"></td>
         <td><input type="text" name="new_waterpump_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_waterpump_minute" placeholder="Minute"></td>
@@ -243,6 +247,7 @@ function addLaptop(button) {
             <input type="checkbox" name="device" value="new_light" checked>
             <label>Laptop</label>
         </td>
+        <td><input type="text" name="quantity"></td>
         <td><input type="text" name="new_laptop_power" value="100W"></td>
         <td><input type="text" name="new_laptop_hour" placeholder="Hour"></td>
         <td><input type="text" name="new_laptop_minute" placeholder="Minute"></td>
